fd015bcc7bdea74b9b21adcd847f12261989263d
Psychogaming03 <aajadkumarinfotech@gmail.com>
2025-07-21 13:17:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-// Script for navigation bar
+<i class="fas fa-bars" id="menu-icon"></i>
+<ul class="navbar">
+    <li><a href="index.html">Home</a></li>
+    <li><a href="shopall.html">Shop All</a></li>
+    <li><a href="product.html">Products</a></li>
+    <li><a href="contact.html">Contact</a></li>
+    <li><a href="cart.html">Cart</a></li>
+</ul>
